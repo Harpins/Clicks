afbbded7a2aa74d33bf6400f04dbeff2643166e8
Igor Derevnin <shroom2207@yandex.ru>
2024-10-26 23:07:57 +0300

    Add files via upload

--- Clicks in console.py
+++ Clicks in console.py
@@ -4,9 +4,6 @@ import requests
 from dotenv import load_dotenv
 
 
-SHORTENED_URL_NETLOCS = ['vk.cc']
-
-
 def shorten_link(token, url):
     parameters = {
         'access_token': token,
@@ -15,67 +12,67 @@ def shorten_link(token, url):
         'private': 0,
     }
     response = requests.post(
-        'https://api.vk.ru/method/utils.getShortLink', data=parameters)
+        'https://api.vk.ru/method/utils.getShortLink', data=parameters, timeout=20)
     response.raise_for_status()
-    try:
-        return response.json()['response']['short_url']
-    except KeyError:
-        return f'Ошибка АPI {response.json()['error']['error_code']}'
-    except Exception as expt:
-        return f'Что-то пошло не так: {expt}'
+    response_data = response.json()
+    if 'response' in response_data:
+        return response_data['response']['short_url']
+    elif 'error' in response_data:
+        return f'Ошибка API {response_data['error']['error_code']}'
 
 
-def count_clicks(token, url, access_key, version='5.199', source='vk_cc', interval='forever', intervals_count=1, extended=0):
-    if short_or_long(url):
-        key = urlparse(url).path[1:]
+def get_url_key(token, url):
+    if is_short_or_long(url):
+        return urlparse(url).path[1:]
     else:
         short_link = shorten_link(token, url)
-        if not short_link:
-            return 'Неверный формат ссылки'
-        key = urlparse(short_link).path[1:]
+        return urlparse(short_link).path[1:]
 
+
+def count_clicks(token, url):
+    key = get_url_key(token, url)
     parameters = {
         'access_token': token,
-        'v': version,
+        'v': '5.199',
         'key': key,
-        'source': source,
-        'access_key': access_key,
-        'interval': interval,
-        'intervals_count': intervals_count,
-        'extended': extended,
+        'source': 'vk_cc',
+        'access_key': '',
+        'interval': 'forever',
+        'intervals_count': 1,
+        'extended': 0,
     }
     response = requests.post(
         'https://api.vk.ru/method/utils.getLinkStats', data=parameters, timeout=20)
     response.raise_for_status()
-    try:
-        return 'Количество переходов по ссылке: ' + str(response.json()['response']['stats'][0]['views'])
-    except IndexError:
-        return 'Нет доступа к статистике'
-    except KeyError:
-        return f'Ошибка АPI {response.json()['error']['error_code']}'
-    except Exception as expt:
-        return f'Что-то пошло не так: {expt}'
+    response_data = response.json()
+    if 'response' in response_data:
+        if response_data['response']['stats']:
+            return f'Количество переходов по ссылке: {response_data['response']['stats'][0]['views']}'
+        else:
+            return 'Нет доступа к статистике'
+    elif 'error' in response_data:
+        return f'Ошибка API {response_data['error']['error_code']}'
 
 
-def short_or_long(url):
+def is_short_or_long(url):
+    shortened_url_netlocs = ['vk.cc']
     parsed_url = urlparse(url)
-    return parsed_url.netloc in SHORTENED_URL_NETLOCS
+    return parsed_url.netloc in shortened_url_netlocs
 
 
 def main():
     load_dotenv('APIToken.env')
-    service_token = os.getenv('service_token')
-    access_key = os.getenv('access_token')
-    if not service_token:
+    vk_service_token = os.getenv('VK_SERVICE_TOKEN')
+    if not vk_service_token:
         print('Не найден токен')
-        exit(1)
-    if not access_key:
-        print('Не найден ключ доступа')
-        exit(1)
-    url = str(input('Введите ссылку: '))
-    if not short_or_long(url):
-        print(shorten_link(service_token, url))
-    print(count_clicks(service_token, url, access_key))
+        return
+    url = input('Введите ссылку: ')
+    try:
+        if not is_short_or_long(url):
+            print(shorten_link(vk_service_token, url))
+        print(count_clicks(vk_service_token, url))
+    except requests.exceptions.HTTPError as error:
+        print(f'Ошибка HTTP {error.response.status_code}')
 
 
 if __name__ == '__main__':
